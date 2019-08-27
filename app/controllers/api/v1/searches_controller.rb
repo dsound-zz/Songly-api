2abb7e57f9c2d72595621b7bcbfd487765ce10ab
dsound-zz <demiansims@gmail.com>
2019-08-26 22:16:39 -0400

    08-26-19

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::SearchesController < ApplicationController
 
     
     def search 
-       byebug 
-        @discog_result = DiscogsApi.search_discogs(params[:q]).titles
-        # @mb_result = MusicbrainzApi.search_brainz(params[:q])
-        # @itunes_result = ItunesApi.search_itunes(params[:q])
-        render json: @discog_result
+        @discog_result = DiscogsApi.search_discogs(params[:q])
+        @mb_result = MusicbrainzApi.search_brainz(params[:q])
+        @itunes_result = ItunesApi.search_itunes(params[:q]).tracks_and_artists
+       
+        render json: {discogs: @discog_result, musicbrainz: @mb_result, iTunes: @itunes_result}
      end 
 end
--- app/models/itunes_api.rb
+++ app/models/itunes_api.rb
@@ -1,14 +1,14 @@
 class ItunesApi
     include HTTParty
 
-    attr_accessor :response, :collection_names, :artist_names, :track_names 
+    attr_accessor :response, :collection_names, :artists, :tracks 
 
 
     def initialize(response) 
         @response = JSON.parse(response) 
-        @collection_names  = @response["results"].map { |k,v| k["collectionName"] }.compact
-        @artist_names = @response["results"].map { |k,v| k["artistName"] }.compact
-        @track_names = @response["results"].map { |k,v| k["trackName"] }.compact
+        @collections  = @response["results"].map { |k,v| k["collectionName"] }.compact
+        @artists = @response["results"].map { |k,v| k["artistName"] }.compact
+        @tracks = @response["results"].map { |k,v| k["trackName"] }.compact
     end 
 
     def self.search_itunes(search)
@@ -20,8 +20,16 @@ class ItunesApi
         end
     end 
 
+    def tracks_and_artists
+        t_and_a = {}
+        @tracks.each_with_index do |track, index|
+            t_and_a[track] = @artists[index]
+         end
+         t_and_a 
+    end
+
     def soundtracks
-        found_soundtracks = @collection_names.select { |cn| cn.include?("Soundtrack") }
+        found_soundtracks = @collections.select { |cn| cn.include?("Soundtrack") }
     end
 
    
